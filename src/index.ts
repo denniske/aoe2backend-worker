45e9a08b3eca36aa3c859accd26eb1c065c058c9
Dennis Keil <dennis.keil10@gmail.com>
2022-10-26 01:32:33 +0200

    legacy2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import {setPrisma} from "./db";
-import {route} from "./routes";
-
 export interface Env {
     // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
     AOE2COMPANION: KVNamespace;
@@ -16,32 +13,15 @@ export interface Env {
     LOCAL: boolean;
 }
 
-// Websockets
-// https://developers.cloudflare.com/workers/learning/using-websockets/
-
 export default {
     async fetch(
         request: Request,
         env: Env,
         ctx: ExecutionContext
     ): Promise<Response> {
-        const { url } = request
-
-        if (url.includes('/favicon.ico')) return new Response('');
-
-        env.LOCAL = url.includes('localhost');
-
-        await setPrisma(env);
-
-        // const result = await fetch('https://www.google.de');
-        // const result = await fetch('http://localhost:4300/');
-        // const result = await fetch('https://pdp.localhost/');
-        // console.log('===>', result.status);
-        // return new Response("Hello World!");
-
-        // console.log('url', url);
-        console.log(env.DATABASE_URL);
-
-        return await route(request, env);
+        const url = new URL(request.url);
+        // url.hostname = 'data2.aoe2companion.com';
+        url.hostname = 'legacy2.aoe2companion.com';
+        return fetch(url.toString(), request);
     },
 };
